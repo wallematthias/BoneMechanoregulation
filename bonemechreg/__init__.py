6f04be83f7baa5bdedd223da3cd83bf29b18fb1f
Matthias Walle <92020703+wallematthias@users.noreply.github.com>
2026-08-29 22:25:07 -0600

    Bump mechanoregulation derivative release

--- bonemechreg/__init__.py
+++ bonemechreg/__init__.py
@@ -12,5 +12,5 @@ __all__ = [
     "__version__",
 ]
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 from bonemechreg.mechreg import MechanoregulationResult, derive_remodelling_labels_from_density, mechanoregulation
